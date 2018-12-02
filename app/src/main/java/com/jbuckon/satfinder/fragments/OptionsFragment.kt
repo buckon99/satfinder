ec978c7a19494aba30280f75b258a7c0f6e5d931
Jack Buckon <jackbuckon@gmail.com>
2018-12-02 15:40:09 -0800

    added updated preferencs layout

--- app/src/main/java/com/jbuckon/satfinder/fragments/OptionsFragment.kt
+++ app/src/main/java/com/jbuckon/satfinder/fragments/OptionsFragment.kt
@@ -11,28 +11,10 @@ import android.view.ViewGroup
 import com.jbuckon.satfinder.R
 import kotlinx.android.synthetic.main.settings_fragment.view.*
 
-class OptionsFragment : Fragment() {
+class OptionsFragment : PreferenceFragmentCompat() {
 
-
-    var listener: View.OnClickListener? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        var f = inflater.inflate(R.layout.settings_fragment, container, false)
-        f.satButton.setOnClickListener(listener)
-        return f
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
-    }
-    fun setOnClickListener(listen: View.OnClickListener) {
-        this.listener = listen
+    override fun onCreatePreferences(bundle: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 
 }
